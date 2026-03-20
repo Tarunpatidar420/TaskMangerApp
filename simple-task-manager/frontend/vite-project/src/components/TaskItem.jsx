656ed93594d2aa6ee5fdc9eafc1558145ded3808
tarunpatidar2805 <tarunpatidarrupariya@gmail.com>
2026-03-20 15:59:08 +0530

    second

--- simple-task-manager/frontend/vite-project/src/components/TaskItem.jsx
+++ simple-task-manager/frontend/vite-project/src/components/TaskItem.jsx
@@ -1,11 +1,15 @@
 function TaskItem({ task, onToggle, onDelete, onEdit }) {
   return (
     <div className={`task-card ${task.status === "completed" ? "completed" : ""}`}>
+      
       <h3>
-        {task.title} {task.status === "completed" ? "✅" : "⏳"}
-      </h3>
+        {task.title}{" "}
+<span className={task.status === "completed" ? "status-completed" : "status-pending"}>
+  {task.status === "completed" ? "Completed" : "Pending"}
+</span>
+        </h3>
 
-      <p>{task.description ? task.description : "No description"}</p>
+      <p>{task.description || "No description"}</p>
 
       <p>
         <strong>Status:</strong> {task.status}
@@ -13,21 +17,37 @@ function TaskItem({ task, onToggle, onDelete, onEdit }) {
 
       <p>
         <strong>Created:</strong>{" "}
-        {task.created_at ? new Date(task.created_at).toLocaleString() : "N/A"}
+        {task.created_at
+          ? new Date(task.created_at).toLocaleString()
+          : "N/A"}
       </p>
 
       <div className="task-buttons">
-        <button onClick={() => onToggle(task._id)}>
+        
+        {/*  Toggle Button (Pink) */}
+        <button
+          className="toggle-btn"
+          onClick={() => onToggle(task._id)}
+        >
           {task.status === "completed" ? "Mark Pending" : "Mark Complete"}
         </button>
 
-        <button className="edit-btn" onClick={() => onEdit(task)}>
+        {/* Edit */}
+        <button
+          className="edit-btn"
+          onClick={() => onEdit(task)}
+        >
           Edit
         </button>
 
-        <button className="delete-btn" onClick={() => onDelete(task._id)}>
+        {/* Delete */}
+        <button
+          className="delete-btn"
+          onClick={() => onDelete(task._id)}
+        >
           Delete
         </button>
+
       </div>
     </div>
   );
